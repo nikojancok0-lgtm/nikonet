f8e3a7fd05fdcf461a66b272c98d872ebd114663
nikojancok0-lgtm <nikojancok0@gmail.com>
2026-08-11 08:57:00 +0700

    Src naila
    Src naila

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,1 @@
-const CACHE='src-naila-v1';
-const FILES=['./','./index.html','./manifest.json','./icon.svg'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE="src-naila-v3";const FILES=["./","./index.html","./manifest.json","./service-worker.js","./icon.svg"];self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting())));self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));self.addEventListener("fetch",e=>e.respondWith(fetch(e.request).then(r=>{if(e.request.method==="GET"){const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy))}return r}).catch(()=>caches.match(e.request))));
